8388c54de8de2d24dcd4a1dbffe3201cc9d8ac76
KhalilHakim <abdukhal@isu.edu>
2016-09-21 10:08:33 -0600

    Initial Start

--- src/LexicalAnalyzer/wwwroot/_references.js
+++ src/LexicalAnalyzer/wwwroot/_references.js
@@ -1,4 +1,6 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="js/angular.min.js" />
+/// <reference path="js/merkle.controller.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
