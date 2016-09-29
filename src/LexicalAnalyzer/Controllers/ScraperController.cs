a6d5e2070babd364144cf8e85c1583b57eca3bf0
David Sandborgh <sanddav2@isu.edu>
2016-09-29 14:35:57 -0600

    added .deb function

--- src/LexicalAnalyzer/Controllers/ScraperController.cs
+++ src/LexicalAnalyzer/Controllers/ScraperController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using HtmlAgilityPack;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,43 +15,28 @@ namespace LexicalAnalyzer.Controllers
     [Route("api/[controller]")]
     public class ScraperController : Controller
     {
-        static async Task<string> RunAsync()
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("https://sourceforge.net");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+        List<HtmlNode> DownLinkList;
+        HtmlNodeCollection DownCollection;
 
-                // HTTP GET
-                HttpResponseMessage response = await client.GetAsync("/");
-                if (response.IsSuccessStatusCode)
-                {
-                    Console.WriteLine("We did it, reddit!\n");
-                    return await response.Content.ReadAsStringAsync();
-                }
-                return "it failed";
-            }
-        }
+        static string myURL = "http://debian.osuosl.org/debian/pool/main/c/c2050/";
+        string DownloadPath = "//a[contains(@href,'.deb')]";
+       // string DownloadPath = "//a[@href]";
+
+
 
         // GET: api/scraper
         [HttpGet]
         public string Get()
         {
-            var testDoc = new HtmlDocument();
-            //   HtmlWeb web = new HtmlWeb();
-            //            testDoc.LoadHtml("http://www.stackoverflow.com");
-            //            string urls = "";
-            //foreach (HtmlNode link in testDoc.DocumentNode.SelectNodes("//a[@href]"))
-            //{
-            //    urls += link.GetAttributeValue("href", string.Empty) + "\n";
-            //}
+    
+            Task<string> task2 = TestContent(myURL);
 
-            Task<string> task = RunAsync();
-            task.Wait();
+            string binaries = GetBinaries(myURL);
 
-            return task.Result;
+            return binaries;
         }
+
+
 
         // GET api/scraper/5
         [HttpGet("{id}")]
@@ -76,5 +62,124 @@ namespace LexicalAnalyzer.Controllers
         public void Delete(int id)
         {
         }
+
+        public string GetBinaries(string URL)
+        {
+            var testDoc = new HtmlDocument();
+            
+
+            Task<string> task = RunBinaryAsync(URL);
+ 
+            task.Wait();
+
+            testDoc.LoadHtml(task.Result);
+            string binaries = "";
+
+            DownCollection = new HtmlNodeCollection(testDoc.DocumentNode);
+
+            foreach (HtmlNode link in testDoc.DocumentNode.SelectNodes(DownloadPath))
+            {
+                binaries += link.GetAttributeValue("href", string.Empty) + "\n";
+
+            }
+
+            //foreach (char s in task.Result)
+            //{
+            //    urls += s;
+            //}
+
+
+            string urlCount = DownCollection.Count().ToString();
+
+            return binaries;
+
+        }
+
+        static async Task<string> TestContent(string URL)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+            client.DefaultRequestHeaders
+                  .Accept
+                  .Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT header
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "relativeAddress");
+            //request.Content = new StringContent("{\"name\":\"John Doe\",\"age\":33}",
+            //                                    Encoding.UTF8,
+            //                                    "application/json");//CONTENT-TYPE header
+
+            Console.WriteLine("Trying Connection\n");
+
+            await client.SendAsync(request)
+                  .ContinueWith(responseTask =>
+                  {
+                      Console.WriteLine("Response: {0}", responseTask.Result);
+                      return "it worked\n";
+                  });
+
+            return "yay";
+        }
+
+        static async Task<string> RunBinaryAsync(string URL)
+        {
+
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(URL);
+                //  client.DefaultRequestHeaders.Accept.Clear();
+                   client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
+                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                // HTTP GET
+             
+                HttpResponseMessage response = await client.GetAsync(URL);
+                HttpContent content = response.Content;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("We did it, reddit!\n");
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+
+                return "it failed";
+
+
+            }
+
+            
+        }
+
+        static async Task<string> RunAsync(string URL)
+        {
+
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(URL);
+                client.DefaultRequestHeaders.Accept.Clear();
+                //   client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+                // client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
+                // client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+                // HTTP GET
+                HttpResponseMessage response = await client.GetAsync(URL);
+                //  HttpContent content = response.Content;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("We did it, reddit!\n");
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+
+                return "it failed";
+            }
+        }
+
     }
 }
