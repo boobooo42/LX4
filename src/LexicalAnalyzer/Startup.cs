6c015e5a83d24e7ed57c8adda301273e5073c7ed
Jonathan Glines <glinjona@isu.edu>
2016-10-15 14:18:15 -0700

    Added simple configuration for the connection string

--- src/LexicalAnalyzer/Startup.cs
+++ src/LexicalAnalyzer/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using LexicalAnalyzer.Database;
 
 namespace LexicalAnalyzer
 {
@@ -29,6 +30,11 @@ namespace LexicalAnalyzer
         {
             // Add framework services.
             services.AddMvc();
+            services.AddOptions();
+
+            // Configure options
+            services.Configure<DatabaseOptions>(
+                    Configuration.GetSection("Database"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
