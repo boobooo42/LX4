db1d048a3be3a4c68b934e6c7aa332bfe64cfd14
William Cao <IceyWill@ICEY>
2016-10-17 08:51:04 -0600

    Moved compute hash function

--- src/LexicalAnalyzer/Database/DatabaseTools.cs
+++ src/LexicalAnalyzer/Database/DatabaseTools.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace LexicalAnalyzer.Database
@@ -23,8 +25,28 @@ namespace LexicalAnalyzer.Database
         public static void InsertIntoDatabase(File file)
         {
             // Create FileRepository object and insert file.
-        }
 
+            String hash=computeHash(file);
+            file.FileHash = hash;
+        }
+        public static string computeHash(File fi)
+        {
+            string stringContent = "";
+            string hashResult = "";
+            SHA256 mySha = SHA256.Create();
+            stringContent = fi.FileContents;
+            hashResult = "";
+            //// Converts the string to bytes
+            byte[] byteData = Encoding.UTF8.GetBytes(stringContent);
+            //// hash data computed by SHA256
+            byte[] hashData = mySha.ComputeHash(byteData);
+            //// puts the bytes into a single readable string with the format
+            foreach (byte v in hashData)
+            {
+                hashResult = hashResult + String.Format("{0:x2}", v);
+            }
+            return hashResult;
+        }
 
     }
 }
