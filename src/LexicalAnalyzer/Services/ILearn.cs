dacfc7af0fa36e403a1c1952c58455bebd926dac
Galen Cochrane <cochgale@isu.edu>
2016-10-26 10:16:35 -0600

    Added ILearn skeleton (just a renamed copy of IScraper)

---
+++ src/LexicalAnalyzer/Services/ILearn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexicalAnalyzer.Services
+{
+    public interface ILearn : ITask, IGuid
+    {
+        string DisplayName {
+            get;
+        }
+        string Description {
+            get;
+        }
+        string ContentType {
+            get;
+        }
+
+        /*
+        IEnumerable<KeyValueProperty> DefaultProperties {
+            get;
+        }
+        */
+
+        IEnumerable<KeyValueProperty> Properties {
+            get;
+        }
+    }
+}
