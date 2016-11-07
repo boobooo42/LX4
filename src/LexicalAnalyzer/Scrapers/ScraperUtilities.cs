41a31a2aaa6753f67c699eeb075f55f86009525c
Isaac Hassel <Isaac Hassel@DESKTOP-GH0Q0NQ>
2016-11-07 08:50:11 -0700

    Moved addCorpusContent

--- src/LexicalAnalyzer/Scrapers/ScraperUtilities.cs
+++ src/LexicalAnalyzer/Scrapers/ScraperUtilities.cs
@@ -400,30 +400,48 @@ namespace LexicalAnalyzer.Scrapers
             Database.DatabaseTools.createFile(System.Text.Encoding.UTF8.GetString(input));
         }
 
-        public static void addCorpusContent(long Id, string Hash, string Name, string Type,
-SqlGuid ScraperGuid, string ScraperType, SqlDateTime DownloadDate, string DownloadURL,
-byte[] Content, ICorpusContext m_context)
+        /// <summary>
+        /// creates a corpus content and adds it to the corpur content repository
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="Hash"></param>
+        /// <param name="Name"></param>
+        /// <param name="Type"></param>
+        /// <param name="ScraperGuid"></param>
+        /// <param name="ScraperType"></param>
+        /// <param name="DownloadDate"></param>
+        /// <param name="DownloadURL"></param>
+        /// <param name="Content"></param>
+        /// <param name="corpContent"></param>
+       public static void addCorpusContent(long Id, string Hash, string Name, string Type,
+    Guid ScraperGuid, string ScraperType, DateTime DownloadDate, string DownloadURL,
+    byte[] Content, ICorpusContext m_context)
         {
             CorpusContent corpContent = new CorpusContent();
 
             /* creates hash of byte array*/
-            using (MD5 md5Hash = MD5.Create())
+            string hashResult = "";
+            SHA256 shaHash = SHA256.Create();
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = shaHash.ComputeHash(Content);
+            hashResult = "";
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            /* for (int i = 0; i < data.Length; i++)
+             {
+                 sBuilder.Append(data[i].ToString("x2"));
+             }*/
+            //// puts the bytes into a single readable string with the format
+            foreach (byte v in data)
             {
-                // Convert the input string to a byte array and compute the hash.
-                byte[] data = md5Hash.ComputeHash(Content);
-                StringBuilder sBuilder = new StringBuilder();
-
-                // Loop through each byte of the hashed data 
-                // and format each one as a hexadecimal string.
-                for (int i = 0; i < data.Length; i++)
-                {
-                    sBuilder.Append(data[i].ToString("x2"));
-                }
-                Hash = sBuilder.ToString(); //change hash to real hash
+                hashResult = hashResult + String.Format("{0:x2}", v);
             }
+            // Hash = sBuilder.ToString(); //change hash to real hash
 
-            corpContent.Id = Id;
-            corpContent.Hash = Hash;
+
+            //corpContent.Id = Id;
+            corpContent.Hash = hashResult;
             corpContent.Name = Name;
             corpContent.Type = Type;
             corpContent.ScraperGuid = ScraperGuid;
