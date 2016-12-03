f275b3f62020def8bf0689f8b44a87757b067dd8
KhalilHakim <abdukhal@isu.edu>
2016-12-03 12:57:23 -0700

    Hooked up Learning Model to status page.

--- src/LexicalAnalyzer/wwwroot/js/statusApp.js
+++ src/LexicalAnalyzer/wwwroot/js/statusApp.js
@@ -2,8 +2,9 @@
 
 statusApp.controller("status", function ($scope, $http, $interval) {
     $scope.scrappers;
+    $scope.learningModels;
 
-    //Gets all scrappers and neural nets. 
+    //Gets all scrappers and learning models. 
     $scope.init = function () {
         $http({
             method: 'get',
@@ -12,12 +13,25 @@ statusApp.controller("status", function ($scope, $http, $interval) {
            .success(function (response) {
                $scope.scrappers = response;
                $scope.setProgress();
+               console.log("Scrappers");
                console.log($scope.scrappers);
-
-               //console.log($scope.literatureList);
            })
            .error(function () {
-               console.log("Failed to get status.")
+               console.log("Failed to get status of scrappers.")
+           });
+
+        $http({
+            method: 'get',
+            url: '/api/learningmodel',
+        })
+           .success(function (response) {
+               $scope.learningModels = response;
+               $scope.setProgress();
+               console.log("Learning Models");
+               console.log($scope.learningModels);
+           })
+           .error(function () {
+               console.log("Failed to get status of learning models.")
            });
     }
 
@@ -27,9 +41,17 @@ statusApp.controller("status", function ($scope, $http, $interval) {
 
     //Set value of each scrapper progress
     $scope.setProgress = function () {
+
+        if ($scope.scrappers != undefined)
         for (var i = 0; i < $scope.scrappers.length; i++) {
             $scope.scrappers[i].Progress *= 100;
             $scope.scrappers[i].Progress = Math.floor($scope.scrappers[i].Progress);
+        }
+
+        if ($scope.learningModels != undefined)
+        for (var i = 0; i < $scope.learningModels.length; i++) {
+            $scope.learningModels[i].Progress *= 100;
+            $scope.learningModels[i].Progress = Math.floor($scope.learningModels[i].Progress);
         }
     }
 
